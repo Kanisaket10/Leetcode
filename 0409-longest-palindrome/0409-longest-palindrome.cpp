4a1624245cc24ef6734361486b0d746edb1e7455
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-09-12 23:44:29 +0530

    Time: 3 ms (57.2%), Space: 9.3 MB (58.38%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,29 +2,44 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         int n=s.size();
-        vector<int> loweralp(26,0), upperalp(26,0);
-
-        for(int i=0; i<n; i++){
-            if(s[i] >= 'a')  loweralp[s[i]-'a']++;
-            else  upperalp[s[i] -'A']++;
+        unordered_map<int,int> mp;
+        for(auto ch: s){
+            mp[ch]++;
         }
         int cnt =0;
-        int odd =0;
-        for(int i=0; i<26; i++){
-            if(loweralp[i]%2==0) cnt +=loweralp[i];
-            else{
-                odd =1;
-                cnt +=loweralp[i]-1;
-            }
-        }
-        for(int i=0; i<26; i++){
-            if(upperalp[i]%2==0) cnt +=upperalp[i];
-            else{
-                odd =1;
-                cnt +=upperalp[i]-1;
-            }
-        }
 
-        return (odd==1)? cnt+1:cnt;
+        for(auto x:mp){
+            if(x.second>1) cnt +=x.second-(x.second%2);
+        }
+        if(cnt<n) cnt+=1;
+        return cnt;
+
+
+
+        // vector<int> loweralp(26,0), upperalp(26,0);
+
+        // for(int i=0; i<n; i++){
+        //     if(s[i] >= 'a')  loweralp[s[i]-'a']++;
+        //     else  upperalp[s[i] -'A']++;
+        // }
+        // int cnt =0;
+        // int odd =0;
+        // for(int i=0; i<26; i++){
+        //     if(loweralp[i]%2==0) cnt +=loweralp[i];
+        //     else{
+        //         odd =1;
+        //         cnt +=loweralp[i]-1;
+        //     }
+        // }
+        // for(int i=0; i<26; i++){
+        //     if(upperalp[i]%2==0) cnt +=upperalp[i];
+        //     else{
+        //         odd =1;
+        //         cnt +=upperalp[i]-1;
+        //     }
+        // }
+
+        // return (odd==1)? cnt+1:cnt;
+
     }
 };
