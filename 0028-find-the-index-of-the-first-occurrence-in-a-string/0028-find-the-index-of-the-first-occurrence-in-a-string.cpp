5fc6547ee7f7b372d94969c52fafdb19278a6c13
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-09-15 15:53:33 +0530

    Time: 3 ms (41.94%), Space: 8 MB (40.17%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,3 +1,4 @@
+              // Using Inbuilt function
 // class Solution {
 // public:
 //     int strStr(string haystack, string needle) {
@@ -12,27 +13,78 @@
 //         return -1;
 //     }
 // };
-// brute force approch
+              // brute force approch
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+         
+//          int m = haystack.size();
+//          int n = needle.size();
+         
+//          for(int i=0; i<=m-n; i++){
+//               int first =i, second =0;
+
+//               while(second < n){
+//                   if(haystack[first] != needle[second]) {break;}
+//                   else{
+//                     first++;
+//                     second++;
+//                   }
+//               }
+//               if(second == n) return first-second;
+//          }
+//          return -1;
+//     }
+// };    
+             // Optimized Using KMP
+               
+
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-         
-         int m = haystack.size();
-         int n = needle.size();
-         
-         for(int i=0; i<=m-n; i++){
-              int first =i, second =0;
+    void findlps(vector<int>& lps, string s){
+       int pre =0, suf =1;
+       
+       while(suf < s.size()){
+          if(s[pre] == s[suf]){
+            lps[suf] = pre+1;
+            suf++; pre++;
+          }
+          else{
+            if(pre ==0){
+                lps[suf]=0;
+                suf++;
+            }
+            else{
+                pre = lps[pre-1];
+                
+            }
+          }
+       }
 
-              while(second < n){
-                  if(haystack[first] != needle[second]) {break;}
-                  else{
-                    first++;
-                    second++;
-                  }
-              }
-              if(second == n) return first-second;
-         }
-         return -1;
     }
-};    
-
+    int strStr(string haystack, string needle) {
+        // using LPS (Longest Prefix Suffix)
+        
+        int m = haystack.size();
+        int n = needle.size();
+        vector<int> lps(n);
+        findlps(lps, needle);
+        int first =0, second=0;
+        while(first < m && second < n){
+            if(haystack[first] == needle[second]){
+               first++; second++;
+            }
+            else{
+                if(second == 0){
+                    first++;
+                }
+                else{
+                    second = lps[second-1];
+                }
+            }
+        } 
+        if(second == n ) return first - second;
+      
+        return -1;
+    }
+};
