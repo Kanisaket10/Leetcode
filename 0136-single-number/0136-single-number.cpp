11907379fbb1189431e659730a3e8ce9c906f2b0
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-09-17 20:17:49 +0530

    Time: 15 ms (52.86%), Space: 19.6 MB (69.55%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,16 +3,24 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
 
-        if(n<=1) return nums[0];
-        // by sorting
-        sort(nums.begin(), nums.end());
-        int i=0;
-        while(i<n){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            }
-            i+=2;
+
+        // optimized
+        int ans=0;
+        for(int i=0; i<n; i++){
+            ans^=nums[i];
         }
-        return nums[n-1];
+      return ans;
+      
+        // if(n<=1) return nums[0];
+        // // by sorting
+        // sort(nums.begin(), nums.end());
+        // int i=0;
+        // while(i<n){
+        //     if(nums[i] != nums[i+1]){
+        //         return nums[i];
+        //     }
+        //     i+=2;
+        // }
+        // return nums[n-1];
     }
 };
