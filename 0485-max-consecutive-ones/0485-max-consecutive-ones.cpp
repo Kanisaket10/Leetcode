e68696362883b266c0189902e894cca4c4794d8a
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-09-17 20:04:24 +0530

    Time: 32 ms (34.58%), Space: 49.1 MB (28.22%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -20,14 +20,6 @@ public:
 
 
   // optimized, making it in O(n) time complexity
-//   int cnt =0, zr=0;
-//   for(int i=0; i<n; i++){
-//     if(nums[i]) cnt++;
-//     if(!nums[i]) zr++;
-//     if(cnt == n) return n;
-//     if(zr == n)  return 0;
-//   }
-//   if(n <= 1 &&  !nums[0]) return 0;
    int second =0;
    int mx=0,temp =0;;
   
@@ -44,21 +36,5 @@ public:
     }
    }
   return mx;
-
-    //     int front =0;
-    //     int last = 1;
-    //     int maxCon = 0;
-    //    for(int i=0; i<nums.size()-1; i++)
-    //     {
-    //         if(!nums[front] && !nums[last]){
-    //           maxCon = max(maxCon,last-front+1);
-    //           front++; 
-    //           last--;
-    //         }
-    //         else if(!nums[front] && nums[last]) front++;
-    //         else if(nums[front] && !nums[last]) last--;
-
-    //     }
-    //   return maxCon;
     }
 };
