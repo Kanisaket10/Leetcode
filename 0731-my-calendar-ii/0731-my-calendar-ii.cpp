81420b0a4fba5ba5237a8271f800c96ff3beca65
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-09-27 17:49:57 +0530

    Time: 124 ms (79.98%), Space: 42.8 MB (11.57%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,43 @@
+class MyCalendarTwo {
+public:
+    map<int, int> events;
+
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        events[start]++;
+        events[end]--;
+        
+        int activeBookings = 0;
+        
+        for (auto &event : events) {
+           
+            activeBookings += event.second;
+            
+            if (activeBookings >= 3) {
+               
+                events[start]--;
+                events[end]++;
+                
+                if (events[start] == 0) {events.erase(start);}
+                if (events[end] == 0) {events.erase(end);}
+
+                return false;
+            }
+
+            if(event.first > end){
+                break;
+            }
+        }
+        
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
