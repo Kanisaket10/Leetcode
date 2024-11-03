236d393657ffb19e30b6c8f0a7105d4ea2469761
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-11-03 23:31:54 +0530

    Time: 4 ms (19.29%), Space: 96.3 MB (25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+
+     // optimize one
+       int maxProfit(vector<int>& prices) {
         int n= prices.size();
         int ptr = INT_MAX; 
-        int a=0;
+        int a=0;   
         int b=0;
 
         for(int i=0; i<n;i++){
@@ -20,4 +22,20 @@ public:
     return b;
         
     }
+    // Brute force
+    // int maxProfit(vector<int>& prices){
+    //    int n = prices.size();
+    //    int ans = INT_MIN;
+
+    //    for(int i=0; i<n-1; i++){
+    //       for(int j=i+1; j<n; j++){
+    //         ans = max(ans, prices[j] - prices[i]);
+    //       }
+    //    }
+    //    if(ans <0)return 0;
+    //    else return ans;
+    // }
+
+   
+  
 };
