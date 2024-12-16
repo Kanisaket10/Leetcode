a51f32106740d685cc1e835a6df10746b624df82
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-17 00:34:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (22.4%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+
+    // Recursive
+    int solve(int i, int j){
+        if(i ==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+
+         int up = solve(i-1, j);
+         int left = solve(i, j-1); 
+         return up+left;
+    }
+
+    // memoisation
+    int solve(int i, int j, vector<vector<int>>& dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int up = solve(i-1, j, dp);
+        int left = solve(i, j-1, dp);
+        return dp[i][j]= up+left;
+
+    }
+
+    int uniquePaths(int m, int n) {
+       // return solve(m-1, n-1);
+
+    //    vector<vector<int>> dp(m, vector<int>(n, -1));
+    //    return solve(m-1,n-1, dp);
+     
+     // Tabulation
+     
+    //  int dp[m][n];
+    //   for(int i=0; i<m; i++){
+    //     for(int j=0; j<n; j++){
+    //         if(i==0 && j==0) dp[i][j] = 1;
+    //         else{
+    //             int up=0, left=0;
+    //             if(i>0)  up = dp[i-1][j];
+    //             if(j>0)  left = dp[i][j-1];
+    //             dp[i][j] = up+left;
+    //         }
+    //     }
+    //   }
+    //   return dp[m-1][n-1];
+      
+      // Space Optimization
+       vector<int>prev(n, 0);
+
+       for(int i=0; i<m; i++){
+        vector<int> curr(n, 0);
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0)  curr[j] =1;
+            else{
+                int up = 0;
+                int left = 0;
+                if(i > 0) up = prev[j];
+                if(j > 0) left = curr[j-1];
+
+                curr[j] = up+ left;
+            }
+        }
+        prev = curr;
+       }
+       return prev[n-1];
+    }
+};
