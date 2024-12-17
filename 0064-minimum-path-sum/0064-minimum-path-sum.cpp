a8e8d79f237cf7a9a9292c1b0be0f49b2af04f43
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-17 21:14:55 +0530

    Time: 1 ms (41.94%), Space: 16.8 MB (5.64%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -25,18 +25,18 @@ public:
        //return solve_me(m-1, n-1, grid, dp);
 
        // Tabulation
-       for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-            if(i==0 && j==0) dp[i][j] = grid[i][j];
-            else{
-                int up =INT_MAX, left= INT_MAX;
-                if(i>0) up = dp[i-1][j];
-                if(j>0) left = dp[i][j-1];
-                dp[i][j] = grid[i][j]+ min(up, left);
-            }
-        }
-       }
-       return dp[m-1][n-1];
+    //    for(int i=0; i<m; i++){
+    //     for(int j=0; j<n; j++){
+    //         if(i==0 && j==0) dp[i][j] = grid[i][j];
+    //         else{
+    //             int up =INT_MAX, left= INT_MAX;
+    //             if(i>0) up = dp[i-1][j];
+    //             if(j>0) left = dp[i][j-1];
+    //             dp[i][j] = grid[i][j]+ min(up, left);
+    //         }
+    //     }
+    //    }
+    //    return dp[m-1][n-1];
 
        // Space Optimization
        vector<int> prev(n, INT_MAX);
