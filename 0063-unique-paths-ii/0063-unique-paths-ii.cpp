3b98999d59b7eadb6a70c0e71cc249a5bf6218f4
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-17 19:30:48 +0530

    Time: 0 ms (100%), Space: 11.9 MB (19.17%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -48,5 +48,23 @@ public:
             }
         }
         return dp[m-1][n-1];
+        
+        //Space Optimization
+        vector<int> prev(n,0);
+        for(int i=0; i<m; i++){
+            vector<int> curr(n, 0);
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j] == 1) curr[j] =0;
+                else if(i ==0 && j==0) curr[j] =1;
+                else{
+                    int up=0, left=0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j]= up+left;
+                }
+            }
+            prev = curr;
+        }
+        prev[n-1];
     }
 };
