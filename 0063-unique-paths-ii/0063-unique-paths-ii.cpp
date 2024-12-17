7b4a473a18e86ad54152cdf3952aea6116bb6976
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-17 19:24:04 +0530

    Time: 0 ms (100%), Space: 12 MB (10.9%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -28,8 +28,25 @@ public:
         int n = obstacleGrid[0].size();
        //Recursion call      return solve(m-1, n-1, obstacleGrid);
        
+       //memoisation call
+        // vector<vector<int>> dp(m,vector<int>(n, -1));
+        // return solve_memo(m-1, n-1, obstacleGrid, dp);
+
+        // Tabulation
         vector<vector<int>> dp(m,vector<int>(n, -1));
-        return solve_memo(m-1, n-1, obstacleGrid, dp);
-        
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j]==1) dp[i][j] =0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0, left=0;
+                  if(i>0) up = dp[i-1][j];
+                  if(j>0) left = dp[i][j-1];
+                  dp[i][j]= up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
