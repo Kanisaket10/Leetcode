57da9223e908f196886966d3d4ceba97908adac1
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-17 19:11:59 +0530

    Time: 0 ms (100%), Space: 11.8 MB (25.18%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    // Recursion
+    int solve(int i, int j, vector<vector<int>>& obstacleGrid){
+       if(i ==0 && j==0) return 1;
+       if(i<0 || j<0) return 0;
+       if(i>=0 && j>=0 && obstacleGrid[i][j] == 1) return 0;
+       int up = solve(i-1, j, obstacleGrid);
+       int left = solve(i, j-1, obstacleGrid);
+       return up +left; 
+    }
+    // Memoisation
+    int solve_memo(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp){
+        if(i>=0 && j>=0 && obstacleGrid[i][j]== 1) return 0;
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = solve_memo(i-1, j,obstacleGrid, dp);
+        int left = solve_memo(i, j-1,obstacleGrid, dp);
+        return dp[i][j] = up+left;
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+       //Recursion call      return solve(m-1, n-1, obstacleGrid);
+       
+        vector<vector<int>> dp(m,vector<int>(n, -1));
+        return solve_memo(m-1, n-1, obstacleGrid, dp);
+        
+    }
+};
