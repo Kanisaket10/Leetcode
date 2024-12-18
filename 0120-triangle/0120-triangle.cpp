bc3fe708455724a3d8fcae75478fbe78d6c2421e
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-18 16:10:13 +0530

    Time: 0 ms (100%), Space: 13.4 MB (10.3%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -24,16 +24,33 @@ public:
       // return memo(0, 0, n, triangle, dp);
 
       // Tabulation
+    //   for(int j=0; j<n; j++){
+    //     dp[n-1][j] = triangle[n-1][j];
+    //   }
+    //   for(int i=n-2; i>=0; i--){
+    //     for(int j=i; j>=0; j--){
+    //         int down = triangle[i][j] + dp[i+1][j];
+    //         int diag = triangle[i][j] + dp[i+1][j+1];
+    //         dp[i][j] = min(down, diag);
+    //     }
+    //   }
+    //   return dp[0][0];
+
+      //Space Optimisation 
+      vector<int> prev(n);
       for(int j=0; j<n; j++){
-        dp[n-1][j] = triangle[n-1][j];
+        prev[j] = triangle[n-1][j];
       }
+
       for(int i=n-2; i>=0; i--){
+        vector<int> curr(n,0);
         for(int j=i; j>=0; j--){
-            int down = triangle[i][j] + dp[i+1][j];
-            int diag = triangle[i][j] + dp[i+1][j+1];
-            dp[i][j] = min(down, diag);
+           int down = triangle[i][j] + prev[j];
+           int diag = triangle[i][j] + prev[j+1];
+           curr[j]= min(down, diag);
         }
+        prev = curr;
       }
-      return dp[0][0];
+      return prev[0];
     }
 };
