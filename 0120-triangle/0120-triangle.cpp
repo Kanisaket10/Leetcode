711b84c372fc0afc3ea2d09f30a6d747dff1e8f1
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-18 16:13:17 +0530

    Time: 0 ms (100%), Space: 13.3 MB (10.3%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -37,13 +37,12 @@ public:
     //   return dp[0][0];
 
       //Space Optimisation 
-      vector<int> prev(n);
+      vector<int> prev(n,0), curr(n,0);
       for(int j=0; j<n; j++){
         prev[j] = triangle[n-1][j];
       }
 
       for(int i=n-2; i>=0; i--){
-        vector<int> curr(n,0);
         for(int j=i; j>=0; j--){
            int down = triangle[i][j] + prev[j];
            int diag = triangle[i][j] + prev[j+1];
