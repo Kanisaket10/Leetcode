f32075d0364f13461acf2781df4cd6864315dbf4
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-29 21:56:18 +0530

    Time: 119 ms (65.34%), Space: 13.4 MB (87.56%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+     //  Memoization
     bool f(int ind, int target,vector<int>& nums, vector<vector<int>>& dp){
        if(target== 0) return 1;
        if(ind == 0) return nums[0] == target;
@@ -13,11 +14,25 @@ public:
     }
     
     bool SubsetSum(vector<int>& nums, int n, int target){
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return f(n-1, target, nums, dp);
-        // Space optimization
-      //  vector<bool> prev(target+1, 0), curr(target+1, 0);
+        // vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        // return f(n-1, target, nums, dp);
         
+        // Space optimization
+        // TC = O(n*target) and SC = O(target)
+       vector<bool> prev(target+1, 0), curr(target+1, 0);
+       prev[0]= curr[0] = true;
+       if(nums[0] <= target) curr[0] = true;
+       
+       for(int i= 1; i<n; i++){
+        for(int k=1; k<= target; k++){
+            bool not_take = prev[k];
+            bool take = false;
+            if(nums[i] <= k) take = prev[k-nums[i]];
+            curr[k] = take| not_take;
+        }
+        prev = curr;
+       }
+        return prev[target];
     }
     bool canPartition(vector<int>& nums) {
        int n = nums.size();
