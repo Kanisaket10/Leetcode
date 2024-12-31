a360e07f176c1bbe74a23faeae1e560d5cb5447c
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-31 22:01:46 +0530

    Time: 16 ms (67.78%), Space: 10.1 MB (86.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    // Memoization
     int ways(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp){
         if(ind == 0) return amount % coins[0] == 0;
 
@@ -11,7 +13,22 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return ways(n-1, amount, coins, dp); 
+        // vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        // return ways(n-1, amount, coins, dp); 
+
+        // Tabulation with Space Optmization
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        for(int T=0; T<= amount; T++) prev[T] =  (T%coins[0] == 0);
+
+        for(int i=1; i<n; i++){
+            for(int T =0; T<= amount; T++){
+                long long not_take = prev[T];
+                long long take =0;
+                if(coins[i] <= T) take = curr[T-coins[i]];
+                curr[T] = not_take+ take;
+            }
+            prev = curr; 
+        }
+        return prev[amount];
     }
 };
