27d214f23b6bcafa44c88592b319bd376b192016
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-12-31 20:40:00 +0530

    Time: 35 ms (50.07%), Space: 21.9 MB (42.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -22,22 +22,41 @@ public:
     //    return ans;
 
        // Tabulation
-       vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+    //    vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+    //    for(int T=0; T<=amount; T++){
+    //     if(T % coins[0] == 0) dp[0][T] = T/coins[0];
+    //     else dp[0][T] = 1e9;
+    //    }
+
+    //    for(int i=1; i<n; i++){
+    //     for(int T = 0; T<= amount; T++){
+    //         int not_take = 0+ dp[i-1][T];
+    //         int take = INT_MAX;
+    //         if(coins[i] <= T) take =1+ dp[i][T-coins[i]];
+    //         dp[i][T] = min(take, not_take);
+    //     }
+    //    }
+    //    if(dp[n-1][amount] >= 1e9) return -1;
+    //    return dp[n-1][amount];
+
+       // Space Optimization
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
        for(int T=0; T<=amount; T++){
-        if(T % coins[0] == 0) dp[0][T] = T/coins[0];
-        else dp[0][T] = 1e9;
+        if(T % coins[0] == 0) prev[T] = T/coins[0];
+        else prev[T] = 1e9;
        }
 
        for(int i=1; i<n; i++){
         for(int T = 0; T<= amount; T++){
-            int not_take = 0+ dp[i-1][T];
+            int not_take = 0+ prev[T];
             int take = INT_MAX;
-            if(coins[i] <= T) take =1+ dp[i][T-coins[i]];
-            dp[i][T] = min(take, not_take);
+            if(coins[i] <= T) take =1+ curr[T-coins[i]];
+            curr[T] = min(take, not_take);
         }
+        prev = curr;
        }
-       if(dp[n-1][amount] >= 1e9) return -1;
-       return dp[n-1][amount];
+       if(prev[amount] >= 1e9) return -1;
+       return prev[amount];
        
     }
 };
