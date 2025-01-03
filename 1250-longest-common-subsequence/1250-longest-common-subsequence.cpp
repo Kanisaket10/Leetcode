18626e0b3cee678cf5d0c89575d1c8865d575c33
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-03 18:53:24 +0530

    Time: 28 ms (42.95%), Space: 27.5 MB (18.1%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -26,5 +26,18 @@ public:
             }
          }
       return dp[n][m];
+
+      // Space Optimization
+      vector<int> prev(m+1,0), cur(m+1, 0);
+       for(int j=0; j<=m; j++) prev[j] = 0;  // first row
+         
+         for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(text1[i-1]== text2[j-1]) cur[j] = 1+prev[j-1];
+                else cur[j] = max(prev[j], cur[j-1]);
+            }
+            prev = cur;
+         }
+      return prev[m];
     }
 };
