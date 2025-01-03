a6105de9446fe39b1c4b247163b2be0824f48507
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-03 21:54:12 +0530

    Time: 10 ms (69.7%), Space: 11.5 MB (87.2%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+     
+     //Tabulation with SPace Optimization
+        vector<int> prev(m+1, 0), cur(m+1, 0);
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]) cur[j] = 1+prev[j-1];
+                else cur[j] = max(cur[j-1], prev[j]);
+            }
+            prev = cur;
+        }
+        return (n+m-2*prev[m]);
+    }
+};
