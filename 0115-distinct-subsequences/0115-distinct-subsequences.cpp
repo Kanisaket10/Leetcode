f960e032918b7b623e37c5332b1415108720ac0d
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-04 18:45:11 +0530

    Time: 29 ms (64.19%), Space: 44.1 MB (29.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -23,23 +23,22 @@ public:
         int n = s.size();
         int m = t.size();
      // return f(n-1, m-1, s, t); 
-      vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-      return memo(n, m, s, t, dp); 
+    //   vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+    //   return memo(n, m, s, t, dp); 
 
       // Tabulation
-    //   vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+      vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
 
-    //   for(int i=0; i<=n; i++) dp[i][0] = 1;
-    //   for(int j=0; j<=m; j++) dp[0][j] = 0;
+      for(int i=0; i<=n; i++) dp[i][0] = 1; // 1st column
 
-    //   for(int i=1; i<=n; i++){
-    //     for(int j=1; j<=m; j++){
-    //         if(s[i-1] == t[j-1]){
-    //             dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-    //         } 
-    //         else dp[i][j] = dp[i-1][j];
-    //     }
-    //   }
-    //   return dp[n][m];
+      for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(s[i-1] == t[j-1]){
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            } 
+            else dp[i][j] = dp[i-1][j];
+        }
+      }
+      return (int)dp[n][m];
     }
 };
