4d39c615b0ddf4f9eccfeb226b947e5a9a21834d
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-04 18:58:44 +0530

    Time: 13 ms (87.98%), Space: 9.1 MB (84.11%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -27,19 +27,19 @@ public:
     //   return memo(n, m, s, t, dp); 
 
       // Tabulation
-      vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
+    //   vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
 
-      for(int i=0; i<=n; i++) dp[i][0] = 1; // 1st column
+    //   for(int i=0; i<=n; i++) dp[i][0] = 1; // 1st column
 
-      for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            if(s[i-1] == t[j-1]){
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-            } 
-            else dp[i][j] = dp[i-1][j];
-        }
-      }
-      return (int)dp[n][m];
+    //   for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=m; j++){
+    //         if(s[i-1] == t[j-1]){
+    //             dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+    //         } 
+    //         else dp[i][j] = dp[i-1][j];
+    //     }
+    //   }
+    //   return (int)dp[n][m];
 
       // Space Optimization
       vector<double> prev(m+1, 0), cur(m+1, 0);
@@ -47,12 +47,12 @@ public:
 
       for(int i=1; i<=n; i++){
         for(int j=1; j<=m; j++){
-            if(s[i-1]==t[j-1]){
-                cur[j] = prev[i-1]+ prev[j];
+            if(s[i-1] == t[j-1]){
+                cur[j] = prev[j-1] + prev[j];
             }
             else cur[j] = prev[j];
         }
-        cur = prev;
+        prev = cur;
       }
       return prev[m];
     }
