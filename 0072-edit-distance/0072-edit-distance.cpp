904b526def89e493f011c06cc3d0c15da2c4d6be
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-06 18:48:00 +0530

    Time: 3 ms (88.72%), Space: 10.5 MB (92.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -16,18 +16,18 @@ public:
         // return f(n, m, word1, word2, dp);
 
         // Tabulation
-        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
 
-        for(int j=0; j<=m; j++) dp[0][j] = j; // first row
-        for(int i=0; i<=n; i++) dp[i][0] = i;  // first column
+        // for(int j=0; j<=m; j++) dp[0][j] = j; // first row
+        // for(int i=0; i<=n; i++) dp[i][0] = i;  // first column
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1+ min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
-            }
-        }
-        return dp[n][m];
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=m; j++){
+        //         if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+        //         else dp[i][j] = 1+ min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+        //     }
+        // }
+        // return dp[n][m];
 
         // space Optimimzation 
          
@@ -39,7 +39,7 @@ public:
         for(int i=1; i<=n; i++){
             cur[0] = i;
             for(int j=1; j<=m; j++){
-                if(word1[i-1] == word2[j-1]) cur[j] = dp[i-1][j-1];
+                if(word1[i-1] == word2[j-1]) cur[j] = prev[j-1];
                 else cur[j] = 1+ min(cur[j-1], min(prev[j], prev[j-1]));
             }
             prev = cur;
