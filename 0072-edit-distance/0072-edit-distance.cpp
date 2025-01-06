64396c181f512dccdca614f0f1284bf23f228400
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-06 18:41:42 +0530

    Time: 7 ms (56.87%), Space: 13.2 MB (34.94%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+   // MEmoization
     int f(int i, int j, string s1, string s2, vector<vector<int>>&dp){
         if(i==0) return j;
         if(j==0) return i;
@@ -11,7 +12,21 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return f(n, m, word1, word2, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return f(n, m, word1, word2, dp);
+
+        // Tabulation
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+
+        for(int j=0; j<=m; j++) dp[0][j] = j;
+        for(int i=0; i<=n; i++) dp[i][0] = i;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1+ min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
