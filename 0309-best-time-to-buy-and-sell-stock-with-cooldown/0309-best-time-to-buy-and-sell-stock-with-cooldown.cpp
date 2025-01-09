04348b8356af7f121ae4ffc285cd74c1c5012e0b
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-01-09 19:34:58 +0530

    Time: 0 ms (100%), Space: 16 MB (51.09%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,7 +10,22 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n =prices.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        return solve(0, 1, n, prices, dp);
+        // vector<vector<int>> dp(n, vector<int>(2,-1));
+        // return solve(0, 1, n, prices, dp);
+     // Tabulation
+         vector<vector<long long>> dp(n+2, vector<long long>(2, 0));
+        dp[n][1] = dp[n][0] = 0;
+
+        for(int i=n-1; i>=0; i--){
+            for(int b=0; b<=1; b++){
+                if(b){
+                    dp[i][b] = max((-prices[i]+dp[i+1][0]), dp[i+1][1]);
+                }
+                else{
+                    dp[i][b] = max((prices[i]+dp[i+2][1]), dp[i+1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
