442a84c66708c4ea6f24283f3e30ec1186ba85fe
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-02-01 17:53:58 +0530

    Time: 0 ms (100%), Space: 21 MB (23.24%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    // dfs by Recurrsion
     void dfs(int node, vector<int> adj[], vector<bool>& visited){
          
         visited[node]=1;
@@ -10,7 +12,32 @@ public:
             }
         }
     }
+    
+    // dfs By Stack 
 
+     void dfs_stack(int node, vector<int> adj[], vector<bool>& visited){
+        visited[node] = 1;
+
+        stack<int> st;
+        st.push(node);
+
+        while(!st.empty()){
+            int ele = st.top();
+            st.pop();
+            
+            // if(!visited[ele]){
+            //     visited[ele] =1;
+            // }
+
+            for(int i=0; i<adj[ele].size(); i++){
+                if(!visited[adj[ele][i]]){
+                    visited[adj[ele][i]] = 1;
+                    st.push(adj[ele][i]);
+                }
+            }
+        }
+     }
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
         int v = isConnected.size();
         vector<int> adj[v];
@@ -29,9 +56,11 @@ public:
         int cnt =0;
 
         for(int i=0; i<v; i++){
-            if(!visited[i]){    
-                dfs(i, adj, visited);
-                 cnt++;
+            if(!visited[i]){  
+
+               // dfs(i, adj, visited);
+                dfs_stack(i, adj, visited);
+                cnt++;
             }
         }
         return cnt;
