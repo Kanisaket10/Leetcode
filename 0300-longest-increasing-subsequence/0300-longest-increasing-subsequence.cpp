7a46b80095d4f9cd63490108933a78c3931bc084
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-02-15 14:49:00 +0530

    Time: 343 ms (27.36%), Space: 571.5 MB (6.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+ // Memoization
     int f(int ind, int prev_ind, int n, vector<int>&nums, vector<vector<int>>& dp){
       if(ind == n) return 0;
       
@@ -13,7 +15,21 @@ public:
 
     int lengthOfLIS(vector<int>& nums) {
        int n = nums.size();
-       vector<vector<int>> dp(n, vector<int>(n+1, -1));
-       return f(0, -1, n, nums, dp);
+    //    vector<vector<int>> dp(n, vector<int>(n+1, -1));
+    //    return f(0, -1, n, nums, dp);
+
+       // Tabulation
+       vector<vector<long long>> dp(n+1, vector<long long>(n+1, 0));
+
+       for(long long ind = n-1; ind >= 0; ind--){
+           for(long long prev_ind = ind-1; prev_ind >=-1; prev_ind--){
+              long long len = dp[ind+1][prev_ind+1];
+              if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
+                   len =max(len, 1+ dp[ind+1][ind+1]);
+                }
+                dp[ind][prev_ind+1] = len;
+           }
+        }
+        return dp[0][0];
     }
 };
