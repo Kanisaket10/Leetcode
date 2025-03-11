1329d20d913dfce6af09d324628a25a4fd8b34d3
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-03-11 19:36:04 +0530

    Time: 0 ms (100%), Space: 14.7 MB (55.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
+    
+    // Better Apporach with TC : O(n) and SC:O(n)
+    vector<int> sum(vector<int>& nums, int target){
+        unordered_map<int, int> mp;
+
+        for(int i=0; i<nums.size(); i++){
+            int need = target - nums[i];  //7 ,2
+            if(mp.find(need) != mp.end()){
+                return {mp[need], i};
+            }
+            mp[nums[i]] = i; // [(2,0),]
+        }
+        return{};
+    }
+
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        int n = nums.size();
-
-        // Brute force
-
-    //    int i,j;
-    //     for(int i=0;i<n-1;i++){
-    //         for(int j=i+1; j<n; j++){
-    //         if((nums[i] + nums[j]) == target){
-    //         return {i,j};
-    //          }
-    //         }
-    //     }
-    //   return {};
-
-     //    optimal approach
-       map<int, int> mp;
-        for(int i=0; i<n; i++)
-        {   int x = target - nums[i];
-            if(mp.find(x) != mp.end()){
-                return {mp[x], i};
-            }
-           
-                mp[nums[i]] = i;
-            
-        }
-        return {-1,-1};
+        // vector<int> ans;
+        // int i=0,j=1;
+        // for(int i=0;i<n-1;i++){
+        //     if((nums[i] + nums[j]) == target){
+        //     ans.push(i);
+        //     ans.push(j);
+        //      }
+        //      else{
+        //         j++;
+        //      }
+        // }
+      //return ans;
+      return sum(nums, target);
     }
 };
