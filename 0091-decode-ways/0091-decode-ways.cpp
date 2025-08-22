93d176b4db70967fef312d93e52666211d66958c
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-08-22 15:35:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (75.07%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -38,7 +38,7 @@ public:
         for(int i=2; i<=n; i++){
 
            int one  = s[i-1]- '0';
-           int two = stoi(s.substr(i-2, 2));
+           int two = (s[i-2] - '0') * 10 + (s[i-1] - '0');
 
            if(one != 0) dp[i] += dp[i-1];
            if(10 <=two && two <= 26) dp[i] += dp[i-2];
