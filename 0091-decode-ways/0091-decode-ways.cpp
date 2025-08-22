f01eb0a052a1607312ef498bd39f292ed2995684
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2025-08-22 15:34:12 +0530

    Time: 2 ms (27.2%), Space: 8.9 MB (53.5%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+   // Memoization
     int dp[101];
     int solve(int ind, string s){
         if(ind == 0) return 1;
@@ -19,15 +20,29 @@ public:
         if(10 <= two && two <= 26){
             ways += solve(ind -2, s);
         }
+
         return dp[ind] = ways;
     }
+
     int numDecodings(string s) {
         if(s[0] == '0') return 0;
         
         int n = s.size();
         memset(dp, -1, sizeof(dp));
        
-        return solve(n, s);
-      
+      //  return solve(n, s);
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        dp[1] = 1;
+       
+        for(int i=2; i<=n; i++){
+
+           int one  = s[i-1]- '0';
+           int two = stoi(s.substr(i-2, 2));
+
+           if(one != 0) dp[i] += dp[i-1];
+           if(10 <=two && two <= 26) dp[i] += dp[i-2];
+        }
+      return dp[n];
     }
 };
