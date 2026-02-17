1bc18bf4081d8ce04858fb980a224da392e9ae6c
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2026-02-18 01:18:19 +0530

    Time: 21 ms (55.94%), Space: 12.5 MB (33.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,11 +1,23 @@
 
 func containsDuplicate(nums []int) bool {
-    sort.Ints(nums)
+    // sort.Ints(nums)
 
-    for i:=0; i<len(nums)-1; i++ {
-        if nums[i] == nums[i+1]{
+    // for i:=0; i<len(nums)-1; i++ {
+    //     if nums[i] == nums[i+1]{
+    //         return true
+    //     }
+    // }
+    // return false
+
+//Optimized
+    seen := make(map[int]bool)
+
+    for _, num := range nums {
+        if seen[num] {
             return true
         }
+        seen[num] = true
     }
+
     return false
 }
